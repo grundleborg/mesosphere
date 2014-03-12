0d04c27c3a7bfa96547dd20263271e288dc80cef
George Goldberg <george@grundleborg.com>
2014-03-12 21:45:36 +0000

    Add urls for blog viewing.

---
+++ mesoblog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from polls import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^article/(?P<article_id>\d+)$', views.article, name='article'),
+)
+
