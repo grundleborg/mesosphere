e0cdd4cef01560fcbd5a388bba4ceeb739f304f8
George Goldberg <george@grundleborg.com>
2014-03-12 22:19:28 +0000

    Fix some silly bugs I made, so blog index view now renders something.

--- mesoblog/views.py
+++ mesoblog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from mesoblog.models import Article
+
+# Index view
+def index(request):
+    articles = Article.objects.all().order_by('-pub_date')[:5]
+    context = {'articles': articles}
+    return render(request, 'mesoblog/index.html', context)
+
+# Article view
+def article(request):
+    context = {}
+    return render(request, 'mesoblog/article.html', context)
