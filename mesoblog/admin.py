f6f805a566475adfeef4de6c19fe978df028e62c
George Goldberg <george@grundleborg.com>
2014-03-13 22:31:28 +0000

    Add categories to blog articles.

--- mesoblog/admin.py
+++ mesoblog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from mesoblog.models import Article
+from mesoblog.models import Article, Category
 
 class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields  = {"slug": ("title",)}
 
 # Register the Models
 admin.site.register(Article, ArticleAdmin)
-
+admin.site.register(Category)
--- mesoblog/models.py
+++ mesoblog/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+# Represents a category which articles can be part of
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name+" ["+str(self.id)+"]"
+
+
 # Article model represents one article in the blog.
 class Article(models.Model):
     title = models.CharField(max_length=255)
@@ -7,6 +15,7 @@ class Article(models.Model):
     contents = models.TextField()
     date_published = models.DateTimeField()
     published = models.BooleanField()
+    categories = models.ManyToManyField(Category)
     
     def __str__(self):
         return self.title+" ["+str(self.id)+"]"
