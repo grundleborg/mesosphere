3afe45447229a349803cbe867a8c903dcafc0d6c
George Goldberg <george@grundleborg.com>
2014-03-13 20:56:58 +0000

    Make blog list and contents show up.

--- mesoblog/views.py
+++ mesoblog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from mesoblog.models import Article
 
 # Index view
 def index(request):
-    articles = Article.objects.all().order_by('-pub_date')[:5]
+    articles = Article.objects.all().order_by('-date_published')[:5]
     context = {'articles': articles}
     return render(request, 'mesoblog/index.html', context)
 
 # Article view
 def article(request, article_id):
-    context = {}
+    article = get_object_or_404(Article, pk=article_id)
+    context = {'article': article}
     return render(request, 'mesoblog/article.html', context)
