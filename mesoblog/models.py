d79fde9a1bceea2ea8bc9aa8d14e5ec66a44600d
George Goldberg <george@grundleborg.com>
2014-03-16 19:44:24 +0000

    Make sure slugs are unique and not integer only.
    This fixes issues #1, #2 & #5

--- mesoblog/models.py
+++ mesoblog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+import random
+import re
+import string
+
 # Represents a category which articles can be part of
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -7,6 +11,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name+" ["+str(self.id)+"]"
+
+    def save(self):
+        # Make sure that the slug is:
+        #   a) not just integers so it doen't look like an ID
+        #   b) unique amongst all other objects of that type
+        
+        # a):
+        if re.match(r'^\d+$', self.slug):
+            self.slug += "_"
+        # b):
+        try:
+            other = Category.objects.get(slug=self.slug)
+            self.slug += "_"
+            self.slug += ''.join(random.sample(string.ascii_lowercase + string.digits, 8))
+        except self.DoesNotExist:
+            pass
+        super(Category, self).save()
 
 
 # Article model represents one article in the blog.
@@ -21,5 +42,23 @@ class Article(models.Model):
     
     def __str__(self):
         return self.title+" ["+str(self.id)+"]"
+
+    def save(self):
+        # Make sure that the slug is:
+        #   a) not just integers so it doen't look like an ID
+        #   b) unique amongst all other objects of that type
+        
+        # a):
+        if re.match(r'^\d+$', self.slug):
+            self.slug += "_"
+        # b):
+        try:
+            other = Article.objects.get(slug=self.slug)
+            self.slug += "_"
+            self.slug += ''.join(random.sample(string.ascii_lowercase + string.digits, 8))
+        except self.DoesNotExist:
+            pass
+
+        super(Category, self).save()
 
 
