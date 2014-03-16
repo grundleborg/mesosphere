38d150e0768d2cc6a18e2c8d3353d414397804b7
George Goldberg <george@grundleborg.com>
2014-03-16 20:26:26 +0000

    Force categories to contain the primary category regardless of UI. Fixes #3

--- mesoblog/admin.py
+++ mesoblog/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.forms import ModelForm
+
 from mesoblog.models import Article, Category
 
+class ArticleAdminForm(ModelForm):
+    class Meta:
+        model = Article
+
+    def clean(self):
+        # Make sure the primary category is included in the list of categories
+        categories = list(self.cleaned_data['categories'])
+        if self.cleaned_data['primary_category'] not in categories:
+            categories.append(self.cleaned_data['primary_category'])
+        self.cleaned_data['categories'] = categories
+        return self.cleaned_data
+
 class ArticleAdmin(admin.ModelAdmin):
+    form = ArticleAdminForm
     prepopulated_fields  = {"slug": ("title",)}
 
 class CategoryAdmin(admin.ModelAdmin):
