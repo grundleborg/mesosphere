0866cb8725be2804ffad47a1334f44264ee59e15
George Goldberg <george@grundleborg.com>
2014-03-17 22:20:38 +0000

    Implement breadcrumbs system and make blog module use it. Fixes #15.

--- mesoblog/views.py
+++ mesoblog/views.py
@@ -1,18 +1,37 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 
 from mesoblog.models import Article, Category
+from mesocore.breadcrumbs import Breadcrumb
 
 # Index view
 def index(request):
     articles = Article.objects.all().order_by('-date_published')[:5]
-    context = RequestContext(request, {'articles': articles})
+
+    # Construct Breadcrumbs
+    b = [
+        Breadcrumb(name="Home"),
+        Breadcrumb(name="Blog"),
+    ]
+
+    context = RequestContext(request, {'articles': articles, 'breadcrumbs': b,})
     return render(request, 'mesoblog/index.html', context)
 
 # Article view
 def article(request, article_id):
     a = get_object_or_404(Article, pk=article_id)
-    context = {'article': a}
+
+    # Construct Breadcrumbs
+    b = [
+        Breadcrumb(name="Home"),
+        Breadcrumb(name="Blog",url=reverse('mesoblog.views.index')),
+        Breadcrumb(name=a.primary_category.name,url=reverse('mesoblog.views.categoryFromSlug',
+            args=(a.primary_category.slug,))),
+        Breadcrumb(name=a.title)
+    ]
+
+    context = {'article': a, 'breadcrumbs': b}
     return render(request, 'mesoblog/article.html', context)
 
 # Article from Slug view
@@ -22,8 +41,17 @@ def articleFromSlug(request, article_slug):
 
 # Category view
 def category(request, category_id):
+    c = Category.objects.get(id=category_id)
     articles = Article.objects.filter(categories__id__contains=category_id).order_by('-date_published')[:5]
-    context = RequestContext(request, {'articles': articles})
+
+    # Construct Breadcrumbs
+    b = [
+        Breadcrumb(name="Home"),
+        Breadcrumb(name="Blog",url=reverse('mesoblog.views.index')),
+        Breadcrumb(name=c.name),
+    ]
+
+    context = RequestContext(request, {'articles': articles, 'breadcrumbs': b})
     return render(request, 'mesoblog/index.html', context)
 
 # Category from Slug view
---
+++ mesocore/breadcrumbs.py
@@ -0,0 +1,5 @@
+class Breadcrumb:
+    def __init__(self, name, url=""):
+        self.name=name
+        if url:
+            self.url=url
