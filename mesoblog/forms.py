d83e22bd5d9bd7493d0ebbb60ee874f199850047
George Goldberg <george@grundleborg.com>
2014-03-29 16:10:57 +0000

    Switch to floppy forms, and make it possible to reply to comments.
    Fixes #30 (Floppy Forms)
    Fixes #9 (Comments Support)

--- mesoblog/forms.py
+++ mesoblog/forms.py
@@ -1,22 +1,27 @@
-from django import forms
-from django.forms import ModelForm
+import floppyforms as forms
 
 from datetime import datetime
 
 from mesoblog.models import Article, Comment
 
-class CommentForm(ModelForm):
+class CommentForm(forms.ModelForm):
     article = forms.ModelChoiceField(label="",
-                                     queryset=Article.objects.all(),
-                                     widget=forms.HiddenInput())
+                                     widget=forms.HiddenInput(),
+                                     queryset=Article.objects.all())
     parent = forms.ModelChoiceField(label="",
-                                    queryset=Comment.objects.all(),
                                     widget=forms.HiddenInput(),
+                                    queryset=Comment.objects.all(),
                                     required=False)
    
     class Meta:
         model = Comment
         fields = ['name', 'email', 'site', 'contents', 'article', 'parent']
+        widgets = {
+                'name': forms.TextInput,
+                'email': forms.EmailInput,
+                'site': forms.URLInput,
+                'contents': forms.Textarea,
+        }
     
     def save(self, commit=True):
         i = super(CommentForm, self).save(commit=False)
