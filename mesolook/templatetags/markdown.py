5d0ba4f033b4927222c7b4f0114d2f524c3389ba
George Goldberg <george@grundleborg.com>
2015-01-30 21:23:40 +0000

    Use blecah instead of markdown safe_mode.

--- mesolook/templatetags/markdown.py
+++ mesolook/templatetags/markdown.py
@@ -1,4 +1,5 @@
 import markdown
+import bleach
 
 from django import template
 from django.template.defaultfilters import stringfilter
@@ -6,24 +7,59 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter(is_safe=True)
+TAGS_SAFE_SUMMARY = [
+        'a',
+        'abbr',
+        'acronym',
+        'b',
+        'blockquote',
+        'code',
+        'em',
+        'i',
+        'li',
+        'ol',
+        'strong',
+        'ul',
+        ]
+
+TAGS_UNSAFE_SUMMARY = TAGS_SAFE_SUMMARY
+TAGS_UNSAFE = TAGS_SAFE_SUMMARY
+
+# Important Reminder:
+# -------------------
+# is_safe=True means Django is allowed to escape any HTML added by the function.
+# is_safe=False mean Django is *not* allowed to escape any HTML added by the function.
+
+@register.filter(is_safe=False)
 @stringfilter
 def markdown_safe(value):
     extensions = ["nl2br", "urlize", "fenced_code", "codehilite",]
+    return markdown.markdown(value, extensions=extensions, enable_attributes=False)
 
-    return mark_safe(markdown.markdown(value,
-                                       extensions,
-                                       safe_mode=True,
-                                       enable_attributes=False))
+@register.filter(is_safe=False)
+@stringfilter
+def markdown_safe_summary(value):
+    extensions = ["nl2br", "urlize", "fenced_code", "codehilite",]
+    return bleach.clean(
+            markdown.markdown(value, extensions=extensions, enable_attributes=False),
+            tags=TAGS_SAFE_SUMMARY,
+            strip=True)
 
 @register.filter(is_safe=False)
 @stringfilter
 def markdown_unsafe(value):
     extensions = ["nl2br", "urlize", "fenced_code", "codehilite",]
+    return bleach.clean(
+            markdown.markdown(value, extensions=extensions, enable_attributes=False),
+            tags=TAGS_UNSAFE,
+            strip=True)
 
-    return markdown.markdown(value,
-                            extensions,
-                            safe_mode=False,
-                            enable_attributes=False)
-
+@register.filter(is_safe=False)
+@stringfilter
+def markdown_unsafe_summary(value):
+    extensions = ["nl2br", "urlize", "fenced_code", "codehilite",]
+    return bleach.clean(
+            markdown.markdown(value, extensions=extensions, enable_attributes=False),
+            tags=TAGS_UNSAFE_SUMMARY,
+            strip=True)
 
